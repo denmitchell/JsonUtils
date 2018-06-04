494b65684ecba9919e52d98504ef93cbe391314f
denmitchell <den.mitchell@yahoo.com>
2018-06-04 18:05:55 -0400

    Added additional support for primitive arrays (including nullables and enums)

--- EDennis.JsonUtils/EDennis.JsonUtils/SafeJsonConverter.cs
+++ EDennis.JsonUtils/EDennis.JsonUtils/SafeJsonConverter.cs
@@ -234,11 +234,28 @@ namespace EDennis.JsonUtils {
 
                 //write the array
                 jw.WriteStartArray();
-                foreach (var obj in list)
-                    SerializeObject(obj, obj.GetHashCode(), null);
+                foreach (var obj in list) {
+                    if (IsSimple(obj.GetType())) {
+                        jw.WriteValue(obj);
+                    } else {
+                        SerializeObject(obj, obj.GetHashCode(), null);
+                    }
+                }
                 jw.WriteEndArray();
 
                 depth--;  //decrement the depth
+            }
+
+
+            private bool IsSimple(Type type) {
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)) {
+                    // nullable type, check if the nested type is simple.
+                    return IsSimple((type.GetGenericArguments()[0]).GetTypeInfo());
+                }
+                return type.IsPrimitive
+                    || type.IsEnum
+                    || type.Equals(typeof(string))
+                    || type.Equals(typeof(decimal));
             }
 
 
