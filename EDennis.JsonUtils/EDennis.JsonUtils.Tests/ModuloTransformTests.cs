05ab48152b2bf6883b86143b3f9251b182925bf2
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2019-10-22 08:35:10 -0400

    Added ModuloTransformTests

---
+++ EDennis.JsonUtils/EDennis.JsonUtils.Tests/ModuloTransformTests.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EDennis.JsonUtils.Tests {
+    public class ModuloTransformTests {
+        private readonly ITestOutputHelper output;
+
+        public ModuloTransformTests(ITestOutputHelper output) {
+            this.output = output;
+        }
+
+        Dictionary<string,ulong>[] ModuloTransform = new Dictionary<string, ulong>[] {
+            new Dictionary<string,ulong> {{"Id",1000}},
+            new Dictionary<string, ulong> { { "Id", 1000 },{"ContactId",100}}
+        };
+
+        internal class Contact {
+            public int ContactId { get; set; }
+            public string Email { get; set; }
+        }
+        internal class PersonWithContact {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public List<Contact> ContactInfo { get; set; } 
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void TestModuloTransform(int index) {
+
+            var expectedJsonFile = $"ModuloTransformTests\\expected{index + 1}.json";
+            var expectedJson = File.ReadAllText(expectedJsonFile);
+            var expectedObject = JToken.Parse(expectedJson).ToObject<List<PersonWithContact>>();
+
+            var inputJsonFile = $"ModuloTransformTests\\expected{index + 1}.json";
+            var inputJson = File.ReadAllText(inputJsonFile);
+            var inputObject = JToken.Parse(expectedJson).ToObject<List<PersonWithContact>>();
+
+            var filter = ModuloTransform[index];
+
+
+            string actual = JsonConvert.SerializeObject(inputObject,
+                    Formatting.Indented, new SafeJsonConverter() {
+                    PropertiesToIgnore = new string[] { },
+                    ModuloTransform = filter
+                });
+            string expected = JsonConvert.SerializeObject(expectedObject,
+                    Formatting.Indented, new SafeJsonConverter() {
+                        PropertiesToIgnore = new string[] { },
+                        ModuloTransform = filter
+                    });
+
+            output.WriteLine($"actual:\n{actual}\n\nexpected:\n{expected}" );
+
+            Assert.Equal(expected, actual);
+
+        }
+
+    }
+}
